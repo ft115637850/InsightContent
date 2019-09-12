781e95b52d3fa8c7731c49db0c34960e906898be
Newton Zou <gentleman000000@hotmail.com>
2019-09-12 18:04:55 +0800

    Add background API

---
+++ InsightContent/Controllers/BackgroundController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InsightContent.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightContent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BackgroundController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult Post([FromForm] BackGroundModel value)
+        {
+            var bgImg = value.ImgFile;
+            return Ok();
+        }
+    }
+}
---
+++ InsightContent/Entities/BackGroundModel.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InsightContent.Entities
+{
+    public class BackGroundModel
+    {
+        public IFormFile ImgFile { get; set; }
+        public string GraphicChartId { get; set; }
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+        public string BgSizeOption { get; set; }
+    }
+}
